bff5a02bccc6047a36c74aae484cdfc991b4e11f
Sabri <sabrirebaine@gmail.com>
2026-02-20 20:26:15 +0100

    modif de testMetrics, on prend les phrases 'Bonsoir le monde' et 'Bonjour le monde' pour le test de 2 phrases différentes. Sinon spaCy supprime les mots vide et on peut avoir des phrases identiques.

--- tests/testMetrics.py
+++ tests/testMetrics.py
@@ -26,9 +26,9 @@ def test_jaccard_empty():
 
 def test_jaccard_metric_class():
     metric = JaccardMetric()
-    result = metric.compute("Bonjour le monde", "Bonjour tout le monde")
+    result = metric.compute("Bonsoir le monde", "Bonjour le monde")
     assert result.name == "jaccard"
-    assert 0 <= result.score <= 1
+    assert 0 < result.score < 1
     print("Test global pour deux phrases différentes, ok.")
 
 test_jaccard_math()
